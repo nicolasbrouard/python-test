f5d23052464488c25e6b526bc6ff8fc37c514334
Nicolas Brouard <nicolas.brouard@gmail.com>
2015-09-15 01:22:32 -0400

    Using urllib.parse

--- thingspeak.py
+++ thingspeak.py
@@ -1,24 +1,30 @@
 import random
 import httplib2
 import time
+import urllib.parse
 
 apiKey = "U9CL7Y7ZUNN7CTLL"
 url = "https://api.thingspeak.com/update"
-conn = httplib2.Http()
+
 
 def doit():
     value = random.randint(1, 100)
     rawValue = random.randint(1, 1000)
-    request = conn.request(url, "POST", "api_key=" + apiKey + "&field1=%d&field2=%d" %(value, rawValue))
-    print(request)
-    print(request[0]['status'])
-    body = request[1]
-    if body == b'0':
+
+    body = urllib.parse.urlencode({'api_key': apiKey,
+                                   'field1': value,
+                                   'field2': rawValue})
+
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+
+    response, content = httplib2.Http().request(url, "POST", body, headers)
+
+    if content == b'0':
         print("failed")
     else:
         print('success')
 
+
 while True:
     doit()
     time.sleep(16)
-
