bd721c093ad1a8f850bcfb298a7fc4ffe5d45cc7
Nicolas Brouard <nicolas.brouard@gmail.com>
2015-10-29 22:35:35 -0400

    GPIO 18 input

--- gpio.py
+++ gpio.py
@@ -5,17 +5,17 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 # set to pull-up (normally closed position)
-GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # setup an indefinite loop that looks for the GPIO 2
 while True:
 
     # door open detected
-    GPIO.wait_for_edge(2, GPIO.RISING)
+    GPIO.wait_for_edge(18, GPIO.RISING)
     print("Button pushed!\n")
 
     # door closed detected
-    GPIO.wait_for_edge(2, GPIO.FALLING)
+    GPIO.wait_for_edge(18, GPIO.FALLING)
     print("Button released!\n")
 
 # cleanup
