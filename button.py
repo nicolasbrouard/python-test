22927f5c3ac263811017e9a47ee367cd5c0d0825
Nicolas Brouard <nicolas.brouard@gmail.com>
2015-10-29 23:46:35 -0400

    Using GPIO.BOTH

--- button.py
+++ button.py
@@ -2,7 +2,7 @@
 import RPi.GPIO as GPIO
 import time
 
-#setup GPIO using Broadcom SOC channel numbering
+# setup GPIO using Broadcom SOC channel numbering
 GPIO.setmode(GPIO.BCM)
 
 # set to pull-up (normally closed position)
@@ -11,25 +11,16 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def my_callback(channel):
-    print "falling edge detected on 18"
-
-
-def my_callback2(channel):
-    print "rising edge detected on 18"
+    print "GPIO 18: ", GPIO.input(18)
 
 
 # when a falling edge is detected on port 17, regardless of whatever
 # else is happening in the program, the function my_callback will be run
-GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime=20)
-
-# when a falling edge is detected on port 23, regardless of whatever
-# else is happening in the program, the function my_callback2 will be run
-# 'bouncetime=300' includes the bounce control written into interrupts2a.py
-GPIO.add_event_detect(18, GPIO.RISING, callback=my_callback2, bouncetime=20)
+GPIO.add_event_detect(18, GPIO.BOTH, callback=my_callback, bouncetime=20)
 
 try:
     time.sleep(100)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
-GPIO.cleanup()           # clean up GPIO on normal exit
+    GPIO.cleanup()  # clean up GPIO on CTRL+C exit
+GPIO.cleanup()  # clean up GPIO on normal exit
